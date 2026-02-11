1de986debe0723b78d8ab46f5fb90bc6407c78fb
its-sakshi <77jainsakshi@gmail.com>
2026-02-11 09:46:07 +0530

    class1
    Updated console log messages for clarity and formatting.

--- class1.js
+++ class1.js
@@ -1,14 +1,14 @@
 //first and last digit of a 2 digit number
 let a =47;
 let first =Math.floor(a/10);
-console.log(`first digit of a 2 digit number${a} is ${first}`);
-console.log(`last digit of a 2 digit number${a} is ${a%10}`);
+console.log(`first digit of a 2 digit number ${a} is ${first}`);
+console.log(`last digit of a 2 digit number ${a} is ${a%10}`);
 
 //first and last digit of a 3 digit number
 let b=567;
 let f=Math.floor(a/100);
-console.log(`first digit of a 3 digit number${a} is ${f}`);
-console.log(`last digit of a 2 digit number${a} is ${a%100}`);
+console.log(`first digit of a 3 digit number ${b} is ${f}`);
+console.log(`last digit of a 2 digit number ${b} is ${b%100}`);
 
 //swapping using third variable 
 let c=5;
@@ -33,7 +33,7 @@ c=d;
 d=temp;
 console.log(`after swapping c=${c} and d=${d}`);
 
-//arithmetic operation
+//arithmetic operation or calculator
 let a =5;
 let b =2;
 console.log(`addition of ${a} and ${b} is ${a+b}`);
@@ -43,23 +43,28 @@ console.log(`division of ${a} and ${b} is ${a/b}`);
 console.log(`modulus of ${a} and ${b} is ${a%b}`);
 
 //area and perimeter of shapes
+//rectangle
 let l=7;
 let b=3;
 console.log(`area of rectangle with length ${l} and breadth ${b} is ${l*b}`);
 console.log(`perimeter of rectangle with length ${l} and breadth ${b} is ${2*(l+b)}`);
 
+//square
 let s =4;
 console.log(`area of square with sides ${s} is ${s*s}`);
 console.log(`perimeter of square with sides ${s} is ${4*s}`);
 
+//circle
 let pi=3.14;
 let r=5;
 console.log(`perimeter of circle with radius ${3} is ${2*pi*r}`);
 console.log(`area of circle with radius ${3} is ${pi*r*r}`);
 
+//triangle
 let h=6;
 let a =2;
 let c = 1;
 let b=3;
 console.log(`area of triangle with base ${b} and height ${h} is ${b*h/2}`);
 console.log(`perimeter of triangle with sides ${a}, ${b} and ${c} is ${a+b+c}`);
+
