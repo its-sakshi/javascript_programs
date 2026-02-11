b7843e066aadbe572b561d8c956bab2471e2cb0a
its-sakshi <77jainsakshi@gmail.com>
2026-02-11 09:57:07 +0530

    datatype

---
+++ class2.js
@@ -0,0 +1,13 @@
+//find datatype 
+let name="sakshi";
+console.log(`type of ${name} is ${typeof(name)}`);
+let roll=45;
+console.log(`type of ${roll} is ${typeof(roll)}`);
+let bool = true;
+console.log(`type of ${bool} is ${typeof(bool)}`);
+let bool1 = false;
+console.log(`type of ${bool1} is ${typeof(bool1)}`);
+let n ='hello';
+console.log(`type of ${n} is ${typeof(n)}`);
+let x= null;
+console.log(`type of ${x} is ${typeof(x)}`);
